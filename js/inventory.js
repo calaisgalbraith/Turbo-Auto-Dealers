13df576c07b608b36752885cea0ff00ed077a39f
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-10-01 22:01:26 -0500

    added slimSelect library & update filtering logic

--- js/inventory.js
+++ js/inventory.js
@@ -25,7 +25,7 @@ async function renderCars (filterOptions = []) {
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">MSRP: $${displayCar.price}</li> 
-                        <li class="list-group-item">Miles: ${displayCar.mileage}</li> 
+                        <li class="list-group-item">Miles: $${displayCar.mileage}</li> 
                         <li class="list-group-item">Color: ${displayCar.color} <i class="${displayCar.colorIcon}" style="color: ${displayCar.colorCode}"></i></li>
                         <li class="list-group-item">Fuel Type: ${displayCar.fuel} <i class="bi ${displayCar.fuelIcon}"></i></li>
                     </ul>
@@ -39,40 +39,39 @@ async function renderCars (filterOptions = []) {
 // Filter cars based on user selected filters
 function filterCars (car, filterOptions) {
     let returnCar = true
-    for (let key in filterOptions) {
-        if (filterOptions[key] === '') { // if no filter applied for field, continue
+    for (const [key, value] of Object.entries(filterOptions)) {
+        if (!value.length) { // if length is 0 (no filter value), continue
             continue
         }
-        if (key === 'price') { // mileage & msrp filtering
-            if (Number(car[key].replace(',', '')) > Number(filterOptions[key])) { //  car price/mileage is > filtered price/mileage
+        if (key === 'price' || key === 'mileage') {// mileage & msrp filtering
+            if (Number(car[key].replace(',', '')) > Number(value[0])) { //  car price/mileage is > filtered price/mileage
                 returnCar = false
                 break
             }
-        } else if (key === 'mileage') { // mileage & msrp filtering
-            if (Number(car[key].replace(',', '')) > Number(filterOptions[key])) { //  car price/mileage is > filtered price/mileage
-                returnCar = false
-                break
+        } else {
+            let matchedFilter = false
+            for (let i = 0; i < value.length; i++) {
+                if (value[i] === car[key].toLowerCase()) { // check if car value matches one of the filter values (ex: filtering by crosstrek or ascent)
+                    matchedFilter = true
+                    break
+                }
             }
-        } else if (filterOptions[key] !== car[key].toLowerCase()) { // check if car value matches filter value
-            returnCar = false
-            break
+            returnCar = matchedFilter // set value to if car matched one of the filters (ex: was crosstrek or ascent)
         }
     }
     return returnCar
 }
 
+
 // Func to filter buttons when filter inventory btn clicked
 $('#filterInventoryBtn').click((e) => {
     e.preventDefault()
-    const filterOptions = {
-        "model": $('#modelSelect').val(),
-        "condition": $('#conditionSelect').val(),
-        "color": $('#colorSelect').val(),
-        "year": $('#yearSelect').val(),
-        "fuel": $('#fuelSelect').val(),
-        "mileage": $('#mileageRange').val(),
-        "price": $('#msrpRange').val(),
+    const filterOptions  = {}
+    for (let i = 0; i < selectIds.length; i++) {
+        filterOptions[selectIds[i].value] = selectIds[i].select.getSelected()
     }
+    filterOptions.mileage = [$('#mileageRange').val()] // set ranges as array to standardize filter data types
+    filterOptions.price = [$('#msrpRange').val()]
     renderCars(filterOptions)
     $('.filterToggle button').html('<i class="bi bi-filter-square-fill"></i>Filter') // change filter btn fill to indicate filtering
     $('#filterInventoryOptions').slideToggle("40")
@@ -89,15 +88,9 @@ $('#clearFiltersBtn').click((e) => {
 
 // Clear form filters, reset selects
 function clearFilters () {
-    $('#modelSelect').prop('selectedIndex',0);
-    $('#conditionSelect').prop('selectedIndex',0);
-    $('#colorSelect').prop('selectedIndex',0);
-    $('#yearSelect').prop('selectedIndex',0);
-    $('#fuelSelect').prop('selectedIndex',0);
-    $('#mileageRange').val('57000')
-    $('#mileageMax').val('57000')
-    $('#msrpRange').val('56000')
-    $('#msrpMax').val('56000')
+    for (let i = 0; i < selectIds.length; i++) {
+        selectIds[i].select.setSelected('') // reset values
+    }
 }
 
 // Toggle filter panel
@@ -114,3 +107,43 @@ $('#mileageRange').on('input', (e) => {
 $('#msrpRange').on('input', (e) => {
     $('#msrpMax').val(e.target.value)
 })
+
+// Slim Select Logic
+const selectIds = [
+    {
+        id: '#modelSelect',
+        placeholderText: 'Select Model',
+        value: "model"
+    },
+    {
+        id: '#conditionSelect',
+        placeholderText: 'Select Condition',
+        value: "condition"
+    },
+    {
+        id: '#colorSelect',
+        placeholderText: 'Select Color',
+        value: "color"
+    },
+    {
+        id: '#yearSelect',
+        placeholderText: 'Select Year',
+        value: "year"
+    },
+    {
+        id: '#fuelSelect',
+        placeholderText: 'Select Fuel',
+        value: "fuel"
+    }
+]
+renderSlimSelects()
+function renderSlimSelects () { // render slim selects for select inputs
+    for (let i = 0; i < selectIds.length; i++) {
+        selectIds[i].select = new SlimSelect({ // add select to track getting/clearing data
+            select: selectIds[i].id,
+            settings: {
+                placeholderText: selectIds[i].placeholderText,
+              }
+        })
+    }
+}
